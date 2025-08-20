75af533e535e61284ef4549684d864e691e75545
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-08-20 18:48:41 -0300

    Feat:Install class-transformer/validator

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateTaskDto {
+  @IsString({message: 'Title must be a string'})
+  @MinLength(3, {message: 'Title must be at least 3 characters long'})
+  @IsNotEmpty({message: 'Title is required'})
   readonly title: string;
+
+  @IsString({message: 'Description must be a string'})
+  @MinLength(5, {message: 'Description must be at least 5 characters long'})
+  @IsNotEmpty({message: 'Description is required'})
   readonly description: string;
 }
